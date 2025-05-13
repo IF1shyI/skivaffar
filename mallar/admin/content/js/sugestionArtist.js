c9a5540ca7caaf5bf814c0e93b9be82669f76630
Arvid <arvid.alund@gmail.com>
2025-05-13 21:12:27 +0200

    allt fungerar med att sugestions av artister

--- mallar/admin/content/js/sugestionArtist.js
+++ mallar/admin/content/js/sugestionArtist.js
@@ -10,22 +10,25 @@ input.addEventListener('input', ()=>{
                 .then(data => {
                     resultsDialog.innerHTML = data;
                     resultsDialog.open = true;
-                });
-        document.querySelector(".artist-suggestion").style.marginBottom = "5rem"
+                    
+                    document.querySelector(".artist-suggestion").style.marginBottom = "5rem";
 
-        const items = document.querySelectorAll(".sug-item")
-        if (items.length === 0){
-            console.log("Inga .sug-item-element hittades.")
-        }
-        items.forEach((item)=>{
-            console.log(item);
-            item.addEventListener("click",()=>{
-                let name = item.textContent;
-                input.value = name;
-                let resultsDialog = document.querySelector(".sug-box");
-                resultsDialog.open = false;
-            })
-        })
+                    const items = document.querySelectorAll(".sug-item");
+
+                    if (items.length === 0) {
+                        console.log("Inga .sug-item-element hittades.");
+                    }
+    
+                    items.forEach((item) => {
+                        item.addEventListener("click", () => {
+                            let name = item.textContent;
+                            input.value = name.trim().replace(/\s+/g, " ");
+                            resultsDialog.open = false;
+                            document.querySelector(".artist-suggestion").style.marginBottom = "1rem";
+                        });
+                    });
+                });
+
     } else {
         document.querySelector('.sug-box').open = false;
     }
